ef9f978533e6a37e01608ce4a2a1526ec8cbd5fd
Misheelbat <p_misheel@me.com>
2022-06-04 20:50:09 +0200

    started project page and project card component

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -5,7 +5,17 @@ import styles from './projects.module.css';
 export default function Projects() {
 	return (
 		<AnimatePage>
-			<section className={styles.projects}>Projects</section>
+			<section className={styles.projects}>
+				<div className={styles.card}>
+					<div className={styles.image}></div>
+					<div className={styles.info}>
+						<p>RECENT PROJECTS</p>
+						<h1>The Color out of Space</h1>
+						<button>Explore details</button>
+						<p>A Multimedia company landing website</p>
+					</div>
+				</div>
+			</section>
 		</AnimatePage>
 	);
 }
