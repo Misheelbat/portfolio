bcbcc5b6543e838887287b596f4cd1b3068231f3
Misheelbat <p_misheel@me.com>
2022-06-08 20:14:18 +0200

    about page header complete and made responsive

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import AnimatePage from 'components/Layout/AnimatePage';
-import profile from 'assets/photo.svg';
+import profile from 'assets/photo.jpg';
 import styles from './about.module.css';
 
 export default function About() {
 	return (
 		<AnimatePage>
 			<section className={styles.about}>
-				<header className={styles.title}>
-					<h1>
-						About <span>john doe</span>
+				<header>
+					<div className={styles.titleContainer}>
+						<div className={styles.aboutName}>
+							<h1>
+								About <span>john doe</span>
+							</h1>
+						</div>
+						<p>
+							I'm a creative developer with years of experience in building
+							products and appealing web experiences.
+						</p>
 						<img src={profile} alt="profile" />
-					</h1>
-					<p>
-						I'm a creative developer with years of experience in building
-						products and appealing web experiences.
-					</p>
+					</div>
 				</header>
+				<div className={styles.ball} />
 			</section>
 		</AnimatePage>
 	);
